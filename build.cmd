126b2586590a0cc4d42c907a383c9712d30f8eb3
David Whitney <david.whitney@euromoneyplc.com>
2015-03-17 16:29:50 +0000

    Init.

---
+++ build.cmd
@@ -0,0 +1,4 @@
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /m:8 /p:Configuration=Release "Ariane.sln"
+
+REM For building symbols
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /m:8 /p:Configuration=Debug "Ariane.sln"
---
+++ package.cmd
@@ -0,0 +1,3 @@
+.nuget\nuget pack "src\SomeClassLibrary\SomeClassLibrary.csproj" -OutputDirectory artifacts -Build -IncludeReferencedProjects -NonInteractive
+
+.nuget\nuget pack "src\SomeOtherClassLibrary\SomeOtherClassLibrary.csproj" -OutputDirectory artifacts -Build -IncludeReferencedProjects -NonInteractive
