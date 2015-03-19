ed548d51e1ac03d633a0f832cada5cc78df3b71f
David Whitney <david.whitney@euromoneyplc.com>
2015-03-19 17:28:01 +0000

    Extensiontest.

---
+++ tests/Ariane.Test.Unit/Drivers/RemoteWebDriver/ArianeConfigurationExtensionsForWebDriverRegistrationTests.cs
@@ -0,0 +1,21 @@
+﻿using Ariane.Drivers.RemoteWebDriver;
+using NUnit.Framework;
+using OpenQA.Selenium.PhantomJS;
+
+namespace Ariane.Test.Unit.Drivers.RemoteWebDriver
+{
+    [TestFixture]
+    public class ArianeConfigurationExtensionsForWebDriverRegistrationTests
+    {
+        [Test]
+        public void WithDriver__GivenDriver_AssignsDriver()
+        {
+            using (var driver = new PhantomJSDriver())
+            {
+                var cfg = new ArianeConfiguration().WithDriver(driver);
+
+                Assert.That(cfg.Driver, Is.InstanceOf<WebDriverBindings>());
+            }
+        }
+    }
+}
